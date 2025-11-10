b9085a3dc0d2a8a8c14f41f0fde422eec2630949
nakayesaa <hajendraherlambang742@gmail.com>
2025-11-11 04:00:30 +0700

    A2 Problem : Maximize MEX

--- Compfest16FR/A2.cpp
+++ Compfest16FR/A2.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(){
+
+  int t;
+  cin >> t;
+  while(t--){
+    int n, x;
+    cin >> n >> x;
+    vector<int>a(n);
+    for(int i=0;i<n;i++) cin >> a[i];
+
+    vector<int> counter(n+1);
+    for(int i = 0; i<counter.size();i++) counter[i] = 0;
+    for(int i = 0; i<a.size();i++) if(a[i] <= n) counter[a[i]]++;
+
+    int result = 0;
+    for(int i=0;i<=n;i++){
+      if(counter[i]==0){
+        result = i;
+        break;
+      }
+      if(counter[i]>1){
+        int move = counter[i]-1;
+        int next = i + x;
+        if(next <= n) counter[next] += move;
+      }
+    }
+
+   cout << result << '\n';
+  }
+
+  return 0;
 }
